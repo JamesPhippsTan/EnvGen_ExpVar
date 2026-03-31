e7d6166d58a70e7471f9d7dc617750ba8db67d5b
JamesPhippsTan <75998662+JamesPhippsTan@users.noreply.github.com>
2026-03-31 10:55:02 +0200

    Update map_trans_veqtl_Ctrl.sh

--- GraVe_Mapping/map_trans_veqtl_Ctrl.sh
+++ GraVe_Mapping/map_trans_veqtl_Ctrl.sh
@@ -5,7 +5,7 @@
 # How many CPUs for this job?
 #$ -pe parallel 1
 # How much RAM per CPU?
-#$ -l h_vmem=48G
+#$ -l h_vmem=150G # tailor depending on your cluster...
 # Number of parallel jobs to be run as part of an array job
 #$ -t 1:438
 #$ -o map_trans_veqtl_Ctrl.out
